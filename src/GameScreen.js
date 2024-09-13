c7e82b1ba07eaa2c7deb246fffdc0002aab8520d
Ricardo Acras <acras@Ricardos-MacBook-Pro-2.local>
2024-09-13 11:41:14 -0300

    altera tipo para numérico

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -105,8 +105,8 @@ function GameScreen({ onEndGame, level, playerName }) {
     <div className="game-screen">
       <h2>{`Pergunta: Quanto é ${question.num1} x ${question.num2}?`}</h2>
       <input
-        type="text"
-        inputMode="numeric"
+        type="tel"
+        inputMode="decimal"
         pattern="[0-9]*"
         value={userAnswer}
         onChange={(e) => setUserAnswer(e.target.value)} // Permite digitar o resultado
