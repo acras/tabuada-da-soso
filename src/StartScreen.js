5e0146014cd3ee932d071044127f488076b4dc2b
Ricardo Acras <acras@Ricardos-MacBook-Pro-2.local>
2024-09-19 14:57:32 -0300

    fundo de capivara na tela inicial

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,33 +1,31 @@
 import React, { useState, useEffect } from "react";
+import "./StartScreen.css"; // Adicionaremos um CSS específico para estilizar
 
 function StartScreen({ onStartGame }) {
   const [name, setName] = useState("");
 
-  // Função para ler o cookie do nome do jogador
   const getCookie = (name) => {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(";").shift();
   };
 
-  // Função para definir o cookie do nome do jogador
   const setCookie = (name, value, days) => {
     const expires = new Date(Date.now() + days * 86400000).toUTCString();
     document.cookie = `${name}=${value}; expires=${expires}; path=/`;
   };
 
   useEffect(() => {
-    // Quando a tela inicial é carregada, tenta ler o nome do jogador do cookie
     const savedName = getCookie("playerName");
     if (savedName) {
-      setName(savedName); // Sugere o nome salvo no cookie
+      setName(savedName);
     }
   }, []);
 
   const handleStart = () => {
     if (name) {
-      setCookie("playerName", name, 30); // Salva o nome do jogador no cookie por 30 dias
-      onStartGame(name); // Inicia o jogo
+      setCookie("playerName", name, 30);
+      onStartGame(name);
     } else {
       alert("Por favor, insira seu nome para começar!");
     }
@@ -35,14 +33,17 @@ function StartScreen({ onStartGame }) {
 
   return (
     <div className="start-screen">
-      <h1>Bem-vindo ao Jogo da Tabuada da Soso!</h1>
-      <input
-        type="text"
-        placeholder="Digite seu nome"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button onClick={handleStart}>Começar Jogo</button>
+      <div className="content">
+        <h1>Bem-vindo ao Jogo da Tabuada da Soso!</h1>
+        <input
+          type="text"
+          placeholder="Digite seu nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        &nbsp;
+        <button onClick={handleStart}>Começar Jogo</button>
+      </div>
     </div>
   );
 }
